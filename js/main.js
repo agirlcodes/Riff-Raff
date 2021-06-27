2a2a732b6c2612277e78bc70c760d3e2785b0af2
Jackeline <jackelineandrea58@hotmail.com>
2021-06-27 18:42:04 +0100

    improvements

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ hideButton.addEventListener('click', function(event) {
     demoGame.style.display = "none";
     commentSection.style.display = "none";
 })
+
+let modal = document.querySelector('#myModal');
+let closeSign = document.querySelector('.close');
+entryText = document.querySelector ('.entry')
+let entries = document.querySelectorAll('.entry');
+let arrayEntries = [... entries];
+
+for (let index=0; index < arrayEntries.length; index++) {
+
+entryText.addEventListener('click', function (event) {
+    modal.style.display = "block";
+})
+
+closeSign.addEventListener('click', function (event) {
+    modal.style.display = "none";
+})
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
