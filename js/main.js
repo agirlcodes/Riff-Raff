6b9f82e231a58efef2700f50ef209dd18e62dd38
Jackeline <jackelineandrea58@hotmail.com>
2021-06-27 16:37:47 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 let playButton = document.getElementById('play-game');
 let hideButton = document.getElementById('hide-game');
 const demoGame = document.querySelector('#demo-game');
+const commentSection = document.querySelector('#comment-section');
 
 playButton.addEventListener('click', function (event) {
     event.preventDefault();
     demoGame.style.display = "block";
+    commentSection.style.display = "block";
 })
 
 hideButton.addEventListener('click', function(event) {
     event.preventDefault();
     demoGame.style.display = "none";
+    commentSection.style.display = "block";
 })
