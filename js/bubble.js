8b0cd70d9d63f50690475bc319d9f05159f0169a
Nat <nataly.angulo530@gmail.com>
2021-06-29 21:39:34 +0100

    de-bugging bubbles

--- js/bubble.js
+++ js/bubble.js
@@ -1,6 +1,4 @@
-// let interactBubble = document.getElementsByClassName("interact");
-let container = document.getElementsByClassName("interact")
-
+let container = document.getElementsByClassName("bubble")
 for (let i = 0; i < container.length; i++) {
     container[i].addEventListener("click", function(){
       this.style.display = "none";
