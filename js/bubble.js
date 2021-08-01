347b1236d2e7d58a509303ef2679e5f3b9e0c1f9
Jackeline <jackelineandrea58@hotmail.com>
2021-08-01 22:35:11 +0100

    scavenger hunt hidden letter removed

--- js/bubble.js
+++ js/bubble.js
@@ -5,8 +5,8 @@ for (let i = 0; i < container.length; i++) {
     });
 } 
 
-let mainBubble = document.getElementById('hide-letter');
-mainBubble.addEventListener('click', function() {
-  // console.log("Hello");
-  alert(`This bubble has given you the letter "S", good work!`)
-})
+// let mainBubble = document.getElementById('hide-letter');
+// mainBubble.addEventListener('click', function() {
+//   console.log("Hello");
+//   alert(`This bubble has given you the letter "S", good work!`)
+// })
